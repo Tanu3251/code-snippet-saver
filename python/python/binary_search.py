a5de9bb8bda07432a9bbedc381746e83d5496fa6
Tanu3251 <beditanushi@gmail.com>
2026-04-08 19:38:40 +0530

    Create binary_search.py
    Added binary search snippet

---
+++ python/python/binary_search.py
@@ -0,0 +1,21 @@
+# Binary Search Algorithm
+
+def binary_search(arr, target):
+    low = 0
+    high = len(arr) - 1
+    
+    while low <= high:
+        mid = (low + high) // 2
+        
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+            
+    return -1
+
+numbers = [1,2,3,4,5,6]
+result = binary_search(numbers,4)
+print("Element found at index:", result)
